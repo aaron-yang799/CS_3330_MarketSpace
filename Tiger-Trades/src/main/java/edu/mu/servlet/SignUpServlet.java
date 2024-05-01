80ca687a20da704948bd83928bcd538750272d60
chesain <cvanteddu1@gmail.com>
2024-04-30 20:44:56 -0500

    Checks if user entery boxes are eaceh filled out

--- Tiger-Trades/src/main/java/edu/mu/servlet/SignUpServlet.java
+++ Tiger-Trades/src/main/java/edu/mu/servlet/SignUpServlet.java
@@ -16,6 +16,23 @@ public class SignUpServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String name = request.getParameter("name").trim(); 
+        String email = request.getParameter("email").trim();
+        String password = request.getParameter("password").trim();
+        String address = request.getParameter("address").trim();
+
+        
+        if (name.isEmpty() || email.isEmpty() || password.isEmpty() || address.isEmpty()) {
+            // Set an attribute indicating error
+            request.setAttribute("error", "All fields must be filled out.");
+
+            // Redirect back to the sign-up page with an error message
+            RequestDispatcher rd = request.getRequestDispatcher("signUp.jsp");
+            rd.forward(request, response);
+            return;
+        }
+		
 		SignUpDao SignUp = new SignUpDao(request.getParameter("name"), request.getParameter("email"), request.getParameter("password"), request.getParameter("address"));
 		SignUp.SignUp();
 		RequestDispatcher rd = request.getRequestDispatcher("signIn.jsp");
